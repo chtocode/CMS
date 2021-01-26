1920d105e97a7666b2167113e4ee0aa797f9bacb
sxlwar <sxlwar@163.com>
2021-01-26 13:35:10 +0800

    chore: switch to https

--- lib/services/api-service.ts
+++ lib/services/api-service.ts
@@ -50,7 +50,7 @@ const getBaseUrl = () => {
   if (process.env.NODE_ENV === 'development') {
     return process.env.NEXT_PUBLIC_API || 'http://localhost:3001/api';
   } else {
-    return 'http://cms.chtoma.com/api';
+    return 'https://cms.chtoma.com/api';
   }
 };
 const baseURL = getBaseUrl();
